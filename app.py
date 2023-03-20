8584329f8fd0908b1e53a713c740624997c1eaf7
Purvi Joshi <purvirekh@gmail.com>
2023-03-20 14:01:23 -0700

    final appication with flask deployment

--- app.py
+++ app.py
@@ -27,7 +27,18 @@ def predict_api():
     output = model.predict(newData)
     print(output[0])
     return jsonify(output[0])
+@app.route("/predict",methods=["POST"])
 
+def predict():
+
+
+    data = [float(x) for x in request.form.values()]
+
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+
+    output = model.predict(final_input)[0]
+
+    return render_template("home.html",prediction_text = "The house price prediction is {}".format(output))
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000,debug=True)
 
